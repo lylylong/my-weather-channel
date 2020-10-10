4d979cc17b61df1166a61d79d6db644481a5060a
Lyly Long <lylylong@gmail.com>
2020-10-10 03:09:04 -0400

    get fetch api for temp and uv index work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,91 @@
-let cityInput = "";
-let cityId = "";
+$(document).ready(function () {
+  //   let cityInput = "";
+  //   let cityId = "";
 
-// click the search button to get the city input
-$("#button-addon2").on("click", function () {
-  console.log("Search button is clicked!");
-  cityInput = $(this).parents().siblings("#city-input").val();
+  // click the search button to get the city input
+  $("#button-addon2").on("click", function () {
+    console.log("Search button is clicked!");
+    // get the input city name
+    let cityInput = $(this).parents().siblings("#city-input").val();
+    console.log("the input is " + cityInput);
+    // fetch the api to get city response
+    function getCurrentWeather() {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          cityInput +
+          "&appid=a8e17bfcb12d79725964af1dd67c506a&units=metric"
+      )
+        .then(function (response) {
+          if (response.ok) {
+            return response.json();
+          } else {
+            alert("Error: " + response.statusText);
+          }
+        })
+        .then(function (responseJsonReturned) {
+          console.log(responseJsonReturned);
+          // get the city id
+          let cityId = responseJsonReturned.id;
+          console.log(cityId);
+          let cityTitle = responseJsonReturned.name;
+          console.log(cityTitle);
+          // local storage the city id and city name
+          localStorage.setItem(cityId, cityTitle);
 
-  console.log(cityInput);
+          let cityList = $("#list-group");
+          let cityBtn = document.createElement("button");
+          cityBtn.classList = "list-group-item";
+          $(cityBtn).val(localStorage.getItem(cityTitle));
+          cityBtn.textContent = cityTitle;
 
-  function getCurrentWeather() {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-        cityInput +
-        "&appid=a8e17bfcb12d79725964af1dd67c506a"
-    )
-      .then(function (response) {
-        if (response.ok) {
-          return response.json();
-        } else {
-          alert("Error: " + response.statusText);
-        }
-      })
-      .then(function (responseJsonReturned) {
-        console.log(responseJsonReturned);
-        let cityId = responseJsonReturned.city.id;
-        console.log(cityId);
-        // local storage the city id and city name
-        localStorage.setItem(cityId, cityInput);
-      });
-  }
+          cityList.append(cityBtn);
+          //   cityBtn.innerHTML = localStorage.getItem("cityInput");
+          //   cityBtn.textContent = localStorage.getItem("cityInput");
 
-  getCurrentWeather();
+          // get the city name display
+          let cityHeader = document.querySelector("#city-title");
+          cityHeader.textContent = cityTitle;
+          // show current date
+          let currentDay = document.querySelector("#current-day");
+          currentDay.textContent = moment().format("dddd, ll");
 
-  // after search, reset the input
-  $(this).parents().siblings("#city-input").val("");
+          let currentTemp = document.querySelector("#current-temp");
+          currentTemp.innerHTML =
+            "&nbsp" + responseJsonReturned.main.temp + " °C";
+
+          let currentHum = document.querySelector("#current-hum");
+          currentHum.innerHTML =
+            "&nbsp" + responseJsonReturned.main.humidity + " %";
+
+          let currentWind = document.querySelector("#wind-speed");
+          currentWind.innerHTML =
+            "&nbsp" + responseJsonReturned.wind.speed + " m/s";
+
+          var lat = responseJsonReturned.coord.lat;
+          var lon = responseJsonReturned.coord.lon;
+
+          fetch(
+            "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+              lat +
+              "&lon=" +
+              lon +
+              "&appid=a8e17bfcb12d79725964af1dd67c506a"
+          )
+            .then(function (response2) {
+              return response2.json();
+            })
+            .then(function (response2JsonReturned) {
+              console.log(response2JsonReturned);
+              console.log(response2JsonReturned.value);
+            });
+        });
+    }
+
+    getCurrentWeather();
+
+    // after search, reset the input
+    $(this).parents().siblings("#city-input").val("");
+  });
+
+  //   $("#list-group .list-group-item").val(localStorage.getItem("6167865"));
 });
