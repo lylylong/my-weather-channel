b86b6e4f6a1f8fc2d95ed1c40bb23e40aeb8d24b
Lyly Long <lylylong@gmail.com>
2020-10-11 02:51:13 -0400

    fetch the 5-day forecast data and append them on the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,6 +57,91 @@ $(document).ready(function () {
           currentWind.innerHTML =
             "&nbsp" + responseJsonReturned.wind.speed + " m/s";
 
+          var lat = responseJsonReturned.coord.lat;
+          var lon = responseJsonReturned.coord.lon;
+
+          // fetch to get the uv index
+          fetch(
+            "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+              lat +
+              "&lon=" +
+              lon +
+              "&appid=a8e17bfcb12d79725964af1dd67c506a"
+          )
+            .then(function (response2) {
+              return response2.json();
+            })
+            .then(function (response2JsonReturned) {
+              console.log(response2JsonReturned);
+              console.log(response2JsonReturned.value);
+              let currentUvIndex = document.querySelector("#uv-index");
+              currentUvIndex.innerHTML = "&nbsp" + response2JsonReturned.value;
+            });
+
+          // fetch to get the forecast for future 5 days
+          fetch(
+            "http://api.openweathermap.org/data/2.5/forecast?q=" +
+              cityInput +
+              "&appid=a8e17bfcb12d79725964af1dd67c506a&units=metric"
+          )
+            .then(function (response) {
+              if (response.ok) {
+                return response.json();
+              }
+            })
+            .then(function (response3JsonReturned) {
+              console.log(response3JsonReturned);
+
+              for (let i = 0; i < response3JsonReturned.list.length; i++) {
+                if (
+                  response3JsonReturned.list[i].dt_txt.indexOf("15:00:00") !==
+                  -1
+                ) {
+                  //   let nextDates = document.querySelector(".next-date");
+                  //   nextDates.innerHTML =
+                  //     "&nbsp" +
+                  //     response3JsonReturned.list[i].dt_txt.split(" ")[0];
+
+                  //   let nextTemps = document.querySelector("#forecast-tempdata");
+                  //   nextTemps.innerHTML =
+                  //     "&nbsp" + response3JsonReturned.list[i].main.temp + " °C";
+                  //   let next1Hums = document.querySelector("#forecast-humdata");
+                  //   next1Hums.innerHTML =
+                  //     "&nbsp" +
+                  //     response3JsonReturned.list[i].main.humidity +
+                  //     " %";
+                  let forecastWarp = document.querySelector("#forecast-warp");
+
+                  let forecastDivs = document.createElement("div");
+                  forecastDivs.classList = "col card text-white bg-primary";
+                  let forecastDivHeader = document.createElement("div");
+                  forecastDivHeader.classList = "card-header next-date";
+                  forecastDivHeader.textContent = response3JsonReturned.list[
+                    i
+                  ].dt_txt.split(" ")[0];
+                  forecastDivs.appendChild(forecastDivHeader);
+                  let forecastDivBody = document.createElement("div");
+                  forecastDivBody.classList = "card-body forecast-details";
+                  let forecastDivTemp = document.createElement("h6");
+                  forecastDivTemp.classList = "card-title";
+                  forecastDivTemp.textContent =
+                    "Temp: " + response3JsonReturned.list[i].main.temp + " °C";
+
+                  let forecastDivHum = document.createElement("h6");
+                  forecastDivHum.classList = "card-title";
+                  forecastDivHum.textContent =
+                    "humidity: " +
+                    response3JsonReturned.list[i].main.humidity +
+                    " %";
+
+                  forecastDivBody.appendChild(forecastDivTemp);
+                  forecastDivBody.appendChild(forecastDivHum);
+                  forecastDivs.appendChild(forecastDivBody);
+                  forecastWarp.appendChild(forecastDivs);
+                }
+              }
+            });
+
           // if the search input before
           if (arrayCity.includes(cityTitle)) {
             return;
@@ -74,26 +159,6 @@ $(document).ready(function () {
           cityBtn.textContent = cityTitle;
 
           cityList.append(cityBtn);
-
-          var lat = responseJsonReturned.coord.lat;
-          var lon = responseJsonReturned.coord.lon;
-
-          fetch(
-            "http://api.openweathermap.org/data/2.5/uvi?lat=" +
-              lat +
-              "&lon=" +
-              lon +
-              "&appid=a8e17bfcb12d79725964af1dd67c506a"
-          )
-            .then(function (response2) {
-              return response2.json();
-            })
-            .then(function (response2JsonReturned) {
-              console.log(response2JsonReturned);
-              console.log(response2JsonReturned.value);
-              let currentUvIndex = document.querySelector("#uv-index");
-              currentUvIndex.innerHTML = "&nbsp" + response2JsonReturned.value;
-            });
         });
     }
 
